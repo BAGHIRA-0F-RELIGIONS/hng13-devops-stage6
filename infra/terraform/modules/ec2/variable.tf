47afa2e3f5576a4482c4c4af70257b2aa99b2a22
bahir <bahirmomodu@gmail.com>
2025-11-29 12:15:49 +0100

    major changes

--- infra/terraform/modules/ec2/variable.tf
+++ infra/terraform/modules/ec2/variable.tf
@@ -28,10 +28,16 @@ variable "ec2_key" {
     type = string  
 }
 
+variable "ec2_key_content" {
+  type        = string
+  description = "Optional public key contents. If set, this string will be used as the public key for aws_key_pair. If empty, the file at var.ec2_key will be read."
+  default     = ""
+}
+
 # variable "public_ip" {
 #     type = string  
 # }
 variable "ssh_private_key_path" {
-  default = "~/.ssh/id_ed25519"
+  default = "~/.ssh/id_rsa"
   type = string
 }
