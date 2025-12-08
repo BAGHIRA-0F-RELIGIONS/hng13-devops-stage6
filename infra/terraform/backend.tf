b69efd1b71eb77008206c328c3d2a8ac9a75c161
bahir <bahirmomodu@gmail.com>
2025-12-08 11:51:29 +0100

    s3 creation

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-   bucket = "hasmoent-tf-backend"
+   bucket = "baghira-tf-backend"
    key    = "tfstate"
    region = "us-east-1"
    dynamodb_table = "terraform-locks"
